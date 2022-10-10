3ffebd30919b006373037790ff79a015ab43ed51
Shohei YOSHIDA <syohex@gmail.com>
2022-10-10 12:56:30 +0900

    Update minimum supported version

--- helm-ag2.el
+++ helm-ag2.el
@@ -5,7 +5,7 @@
 ;; Author: Shohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-ag2
 ;; Version: 0.58
-;; Package-Requires: ((emacs "28.1") (helm "3.6.0"))
+;; Package-Requires: ((emacs "28.2") (helm "3.6.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
